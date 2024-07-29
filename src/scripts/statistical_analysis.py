5f4d0f37a25f62c26704e77c00400ce3e699981d
abdallah <abdallah.abouabdallah.bmt@gmail.com>
2024-07-29 17:34:22 +0200

    Fix import

--- src/scripts/statistical_analysis.py
+++ src/scripts/statistical_analysis.py
@@ -1,7 +1,6 @@
 import sys
 import os
-sys.path.insert(1, os.path.abspath('../configs'))
-import config # type: ignore
+from configs import config
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
